1700b6bfeaa85f6ccfa85d4110c832880c3f6a43
Daniil <94884910+Filienko@users.noreply.github.com>
2024-05-13 18:15:23 -0700

    Modifying tests

--- tests/migration_list_test.py
+++ tests/migration_list_test.py
@@ -97,17 +97,14 @@ def test_check_consistency_valid(linked_list):
     assert linked_list.check_consistency()
 
 
-def test_check_consistency_cycle(linked_list):
-    """Test consistency check when head is None"""
-    linked_list.head.next_node = linked_list.head.prev_node.prev_node
-    with pytest.raises(RuntimeError, match="Cycle detected in the sequence"):
-        linked_list.check_consistency()
+def test_check_dictionary_consistency_valid(linked_list, nodes_references):
+    assert linked_list.check_dictionary_consistency(nodes_references)
 
 
 def test_check_consistency_no_next_reference(linked_list):
     """Test consistency check when a node has no next reference and is not the head"""
     linked_list.head.prev_node.prev_node.next_node = None
-    with pytest.raises(RuntimeError, match="Consistency error: find a node without a next reference that is not the head"):
+    with pytest.raises(RuntimeError, match="Consistency error: node references are not consistent"):
         linked_list.check_consistency()
 
 
@@ -116,20 +113,6 @@ def test_check_consistency_inconsistent_references(linked_list):
     linked_list.head.prev_node.prev_node.next_node = linked_list.head
     with pytest.raises(RuntimeError, match="Consistency error: node references are not consistent"):
         linked_list.check_consistency()
-
-
-def test_check_dictionary_consistency_valid(linked_list, nodes_references):
-    assert linked_list.check_dictionary_consistency(nodes_references)
-
-
-def test_check_dictionary_consistency_cycle(linked_list, nodes_references):
-    node1 = nodes_references["node1"]
-
-    linked_list.head.next_node = node1  # Creating a cycle
-
-    with pytest.raises(RuntimeError) as exc_info:
-        linked_list.check_dictionary_consistency(nodes_references)
-    assert str(exc_info.value) == "Cycle detected in the list"
 
 
 def test_check_dictionary_consistency_missing_reference(linked_list, nodes_references):
